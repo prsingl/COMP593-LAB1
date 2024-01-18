acc395bdcf636ff5d3244638c46e009b7b4d6757
Priyanshu singla <priyanshu.singla@flemingcollege.ca>
2024-01-17 21:43:37 -0500

    Modified both script files
    -Deleted print from script1.py
    -Added comment to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Print lines from Zen of Python
 print("Beautiful is better than ugly.")
-print("Simple is better than complex.")
--- script2.py
+++ script2.py
@@ -1,2 +1,3 @@
+#Pritn lines from Zen of Python.
 print("flat is better than nested.")
 print("Now is better than never.")
